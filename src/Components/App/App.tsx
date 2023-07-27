0a7661f685f3d6d91f8154823ef608e55313b212
Max Skorokhodov <skormaksim@mail.ru>
2023-07-27 13:33:26 +0300

    feat: добавить NavbarNested

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { NavbarNested } from '../NavbarNested/NavbarNested';
+
+function App() {
+  return (
+    <div className="App">
+     <NavbarNested/>
+    </div>
+  );
+}
+
+export default App;
