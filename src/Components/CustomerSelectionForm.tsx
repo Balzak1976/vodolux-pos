96401cc4972fb69de3a7699244278308d5908b0d
Max Skorokhodov <skormaksim@mail.ru>
2023-07-31 14:40:17 +0300

    ref: расширить список покупателя avatar, description

--- src/Components/CustomerSelectionForm.tsx
+++ src/Components/CustomerSelectionForm.tsx
@@ -1,16 +1,48 @@
-import {IconSearch  } from '@tabler/icons-react';
-import { Select,rem } from '@mantine/core';
-import { useState } from 'react';
+import { IconSearch } from '@tabler/icons-react';
+import { Select, rem, Group, Avatar, Text } from '@mantine/core';
+import { useState, forwardRef } from 'react';
 import { customers } from '../data/customerData';
+
+interface ItemProps extends React.ComponentPropsWithoutRef<'div'> {
+  image: string;
+  label: string;
+  description: string;
+}
+
+const SelectItem = forwardRef<HTMLDivElement, ItemProps>(({ image, label, description, ...others }: ItemProps, ref) => (
+  <div ref={ref} {...others}>
+    <Group noWrap>
+      <Avatar src={image} />
+
+      <div>
+        <Text size='sm'>{label}</Text>
+        <Text size='xs' opacity={0.65}>
+          {description}
+        </Text>
+      </div>
+    </Group>
+  </div>
+));
 
 export function CustomerSelectionForm() {
   const [data, setData] = useState(customers);
+
+  const createCustomer = (query: string) => {
+    const item = {
+      image: 'https://img.icons8.com/clouds/256/000000/futurama-bender.png',
+      label: query,
+      value: query,
+      description: 'сантехник',
+    };
+    setData((current) => [...current, item]);
+    return item;
+  };
 
   return (
     <Select
       p='md'
       pb={rem(9)}
-      icon={<IconSearch size="1rem" />}
+      icon={<IconSearch size='1rem' />}
       styles={{ rightSection: { pointerEvents: 'none' } }}
       maxDropdownHeight={400} // add scroll
       data={data}
@@ -19,11 +51,12 @@ export function CustomerSelectionForm() {
       creatable
       clearable
       getCreateLabel={(query) => `+ Создать покупателя: ${query}`}
-      onCreate={(query) => {
-        const item = { value: query, label: query };
-        setData((current) => [...current, item]);
-        return item;
-      }}
+      onCreate={createCustomer}
+      itemComponent={SelectItem}
+      filter={(value, item) =>
+        (item.label ? item.label.toLowerCase().includes(value.toLowerCase().trim()) : false) ||
+        item.description.toLowerCase().includes(value.toLowerCase().trim())
+      }
     />
   );
 }
--- src/data/customerData.js
+++ src/data/customerData.js
@@ -1,4 +1,27 @@
 export const customers = [
-  { value: 'customer1', label: 'Розничный покупатель' },
-  { value: 'customer2', label: 'Сантехник' },
+  {
+    image: 'https://img.icons8.com/clouds/256/000000/futurama-bender.png',
+    label: 'Bender Bending Rodríguez',
+    value: 'Bender Bending Rodríguez',
+    description: 'Fascinated with cooking',
+  },
+
+  {
+    image: 'https://img.icons8.com/clouds/256/000000/futurama-mom.png',
+    label: 'Carol Miller',
+    value: 'Carol Miller',
+    description: 'One of the richest people on Earth',
+  },
+  {
+    image: 'https://img.icons8.com/clouds/256/000000/homer-simpson.png',
+    label: 'Homer Simpson',
+    value: 'Homer Simpson',
+    description: 'Overweight, lazy, and often ignorant',
+  },
+  {
+    image: 'https://img.icons8.com/clouds/256/000000/spongebob-squarepants.png',
+    label: 'Spongebob Squarepants',
+    value: 'Spongebob Squarepants',
+    description: 'Not just a sponge',
+  },
 ];
