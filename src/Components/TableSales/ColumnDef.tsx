2d7aa823b894ee97c529292625ba767c83dfa5c9
Max Skorokhodov <skormaksim@mail.ru>
2023-09-11 15:08:07 +0300

    ref: настроить сортировку и видимость колонок

--- src/Components/TableSales/ColumnDef.tsx
+++ src/Components/TableSales/ColumnDef.tsx
@@ -2,34 +2,39 @@ import { ColumnDef } from '@tanstack/react-table';
 import { formatCurrency } from '../../utils/formatCurrency';
 import { TableCell } from './TableCell';
 
-export type Columns = {
+type GoodColumns = {
 	name: string;
 	qty: number;
 	price: number;
 	stocks: number;
 };
 
-export const goodColumns: ColumnDef<Columns>[] = [
+export const goodColumns: ColumnDef<GoodColumns>[] = [
 	{
 		accessorKey: 'name',
 		header: 'Название',
+		enableHiding: false
 	},
 	{
 		accessorKey: 'qty',
 		header: 'Кол-во',
 		cell: TableCell,
+		enableSorting: false
 	},
 	{
 		accessorKey: 'price',
 		header: 'Цена',
 		cell: TableCell,
+		enableSorting: false
 	},
 	{
 		accessorFn: row => row.qty * row.price,
 		header: 'Итог',
+		enableSorting: false
 	},
 	{
 		accessorKey: 'stocks',
 		header: 'Склад',
+		enableSorting: false
 	},
 ];
