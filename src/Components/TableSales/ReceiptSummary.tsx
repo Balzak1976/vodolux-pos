20746a41e11553896cf1b45098ff141e8f397d21
Max Skorokhodov <skormaksim@mail.ru>
2023-09-18 11:32:58 +0300

    feat: сверстать ReceiptSummery

--- src/Components/TableSales/ReceiptSummary.tsx
+++ src/Components/TableSales/ReceiptSummary.tsx
@@ -3,9 +3,15 @@ import { formatCurrency } from '../../utils/formatCurrency';
 
 export function ReceiptSummary() {
 	return (
-		<Flex p='xs' gap='xs' justify='space-between' bg='#e7f5ff'>
-			<Text color='blue'>{'позиций: '}<Text span fw='bold'>{24}</Text></Text>
-			<Text color='blue' fz='xl'>{'Подитог: '}<Text span fw='bold'>{formatCurrency(1200)}</Text></Text>
+		<Flex p='xs' gap='xs' justify='space-between' align='end' bg='#e7f5ff'>
+      <Text color='blue'>{'позиций: '}<Text span fw='bold'>{24}</Text></Text>
+      <Text color='blue' fz='md' align='right'>{'скидка: '}
+        <Text span fw='bold'>{`${formatCurrency(1200)} (${10}%)`}</Text>
+      </Text>
+      <Flex direction='column'>
+			  <Text color='blue' fz='md' align='right'>{'подитог: '}<Text span fw='bold'>{formatCurrency(1200)}</Text></Text>
+			  <Text color='blue' fz='xl' align='right'>{'итог: '}<Text span fw='bold'>{formatCurrency(1200)}</Text></Text>
+      </Flex>
 		</Flex>
 	);
 }
